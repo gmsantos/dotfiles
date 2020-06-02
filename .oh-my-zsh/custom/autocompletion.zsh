d93869aa6b06b7d402a637d85ac0002ee2c5df40
Gabriel Machado <gabriel.ms1@hotmail.com>
2020-06-02 13:44:57 -0300

    Add some comments in zsh autosuggest custon config

--- .oh-my-zsh/custom/autocompletion.zsh
+++ .oh-my-zsh/custom/autocompletion.zsh
@@ -1,2 +1,10 @@
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="${ZSH_AUTOSUGGEST_LIGHT:-fg=8}"
+# This configuration improve autocomplete contrast in light themes.
+# Define an envvar named ZSH_AUTOSUGGEST_LIGHT with "fg=250" as the value.
+#
+# I use this especially in vscode, becasue I like light themes :)
+#
+# "terminal.integrated.env.linux": {
+#     "ZSH_AUTOSUGGEST_LIGHT": "fg=250"
+# }
 
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="${ZSH_AUTOSUGGEST_LIGHT:-fg=8}"
