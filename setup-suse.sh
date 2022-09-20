5a8614e34716374deea6a41fade76b7b8788d422
gmsantos <gabriel.ms1@hotmail.com>
2022-09-20 22:38:34 +0200

    add SUSE-based setup

---
+++ setup-suse.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -x
+
+DOT_FILES_DIR=$PWD
+
+sudo zypper install -y zsh git rsync
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sudo usermod --shell /bin/zsh $USER
+
+cd $HOME/.oh-my-zsh/custom/plugins
+
+git submodule add -f https://github.com/djui/alias-tips
+git submodule add -f https://github.com/zsh-users/zsh-autosuggestions
+git submodule update --init
+
+cd $DOT_FILES_DIR
+
+rsync --exclude ".git/" --exclude "setup.sh" -av . $HOME
